d2b1b284808e529c5ae4c97d08c141ac1999aaef
Rushabh Babre <76030589+stonerrb@users.noreply.github.com>
2023-04-14 22:22:31 +0530

    mongoose model for Food_profiles
    Only food_profiles model are added

---
+++ src/models/food_items.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const crypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const foodItems = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String
+    },
+    category:{
+        type:String,
+        required:true,
+    }
+})
+
+const FoodItems = mongoose.model("FoodItems", foodItems);
+module.exports = FoodItems;
