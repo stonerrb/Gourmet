c33e4e1c3e924be8ec7030a274bda71996eb59c9
Manish Gudewar <202152322@iiitvadodara.ac.in>
2023-04-14 23:30:11 +0530

    Hello

--- gourmet-app/src/App.js
+++ gourmet-app/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import Login from "./Pages/Menu";
 
 const App = () => {
-    return (
-        <div>
-            <Navbar/>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
