f8fc8b984c174b90f6c8f1ab67ef06f686d2d5c9
Manish Gudewar <202152322@iiitvadodara.ac.in>
2023-04-14 23:23:35 +0530

    Fixed `````

--- gourmet-app/src/Components/FoodCard.js
+++ gourmet-app/src/Components/FoodCard.js
@@ -1,35 +1,54 @@
-import React, { useState } from 'react';
-import { Card, CardActionArea, CardMedia, CardContent, Typography, IconButton, Button } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import React, { useState } from "react";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  IconButton,
+  Button,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { orange } from "@mui/material/colors";
+
+const { palette } = createTheme();
+const { augmentColor } = palette;
+const createColor = (mainColor) => augmentColor({ color: { main: mainColor } });
 
 const theme = createTheme({
+  palette: {
+    anger: createColor("#F40B27"),
+    apple: createColor("#5DBA40"),
+    steelBlue: createColor("#5C76B7"),
+    violet: createColor("#BC00A3"),
+  },
   root: {
     maxWidth: 345,
-    margin: '1rem',
+    margin: "1rem",
   },
   media: {
     height: 200,
   },
   button: {
-    margin: '0.5rem',
+    margin: "0.5rem",
   },
   palette: {
     primary: {
-      main: '#000000',
+      main: "#000000",
     },
     secondary: {
-      main: '#f50000',
+      main: "#f50000",
     },
     text: {
-      secondary: '#757575',
+      secondary: "#757575",
     },
   },
 });
 
-const FoodCard = ({ item }) => {
-  const [quantity, setQuantity] = useState(0);
+const FoodCard = ({ foodItems }) => {
+  const [quantity, setQuantity] = useState(1);
   const [wishlist, setWishlist] = useState(false);
 
   const handleAddToWishlist = () => {
@@ -38,26 +57,23 @@ const FoodCard = ({ item }) => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Card item xs={12} md={8} lg={8} xl={8} >
+      <Card item xs={12} md={8} lg={8} xl={8}>
         <CardActionArea>
-          <CardMedia
-            component="img"
-            height="200"
-            image={item.image}
-          />
+          <CardMedia component="img" height="200" image={foodItems.image} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
-              {item.title}
+              {foodItems.title}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              {item.description}
+              {foodItems.description}
             </Typography>
           </CardContent>
         </CardActionArea>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ display: "flex", alignItems: "center" }}>
             <Button
               variant="outlined"
+              sx={{ borderRadius: 28, m: 0.5 }}
               onClick={() => setQuantity(Math.max(1, quantity - 1))}
             >
               -
@@ -65,14 +81,18 @@ const FoodCard = ({ item }) => {
             <Typography>{quantity}</Typography>
             <Button
               variant="outlined"
+              sx={{ borderRadius: 28, m: 0.5 }}
               onClick={() => setQuantity(quantity + 1)}
             >
               +
             </Button>
           </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <IconButton aria-label="add to favorites" onClick={handleAddToWishlist}>
-              <FavoriteIcon color={wishlist ? 'secondary' : 'action'} />
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <IconButton
+              aria-label="add to favorites"
+              onClick={handleAddToWishlist}
+            >
+              <FavoriteIcon color={wishlist ? "secondary" : "action"} />
             </IconButton>
             <IconButton aria-label="add to cart">
               <AddShoppingCartIcon />
--- gourmet-app/src/Pages/Wishlist.js
+++ gourmet-app/src/Pages/Wishlist.js
@@ -2,15 +2,15 @@ import { Container, Grid } from "@mui/material";
 import FoodCard from "../Components/FoodCard";
 const Wishlist = () => {
   return (
-    <Container>
-      <Grid container spacing={2}>
-        <Grid item>
+    <Container lg={1}>
+      <Grid container spacing={2} sx={{ m: "90px" }}>
+        <Grid item xl={3} lg={3}>
           <FoodCard
-            item={{
-              title: "Food",
-              description: "Food Description",
+            foodItems={{
+              title: "Chicken Burger",
+              description: "Chicken Burger with cheese and lettuce",
               image:
-                "https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg",
+                "https://images.unsplash.com/photo-1616486490928-8e1b0e1b2b1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
             }}
           />
         </Grid>
