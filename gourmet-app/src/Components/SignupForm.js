a665377abcbc7c43f1fc9137036a70ec65663b06
Manish Gudewar <202152322@iiitvadodara.ac.in>
2023-04-15 23:01:48 +0530

    Port Changes

--- gourmet-app/src/Components/SignupForm.js
+++ gourmet-app/src/Components/SignupForm.js
@@ -1,66 +1,99 @@
-
-import React, { useState } from 'react';
-import { TextField, FormControl, InputAdornment, InputLabel, OutlinedInput, IconButton, Input, Button, colors } from "@mui/material";
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import React, { useState } from "react";
+import {
+  TextField,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  IconButton,
+  Input,
+  Button,
+  colors,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 export default function SignupForm() {
-    const [showPassword, setShowPassword] = React.useState(false);
-    
+  const [showPassword, setShowPassword] = React.useState(false);
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
-    const loginValues = {
-        email: '',
-        password: '',
-    };
-
-    const signupValues = {
-        name: '',
-        email: '',
-        number: '',
-        password: '',
-    };
-    const [lvalue, setLValue] = useState(loginValues);
-    const [svalue, setSValue] = useState(signupValues);
+  const signupValues = {
+    name: "",
+    email: "",
+    number: "",
+    password: "",
+  };
+  const [svalue, setSValue] = useState(signupValues);
   return (
-    
     <div className="signup-form">
-                    <form id="form2">
-                        <h1>Sign Up</h1>
-                        <div className="input-container">
-                            <TextField sx={{ m: 1, width: '25ch' }} className="input" id="standard-basic" label="Name" variant="standard" value={svalue.name} onChange={(e) => setSValue({...svalue, name: e.target.value})} />
-                            <TextField sx={{ m: 1, width: '25ch' }} className="input" id="standard-basic" label="Email" variant="standard" value={svalue.email} onChange={(e) => setSValue({...svalue, email: e.target.value})}/>
-                            <TextField sx={{ m: 1, width: '25ch' }} className="input" id="standard-basic" label="Phone No." variant="standard" value={svalue.number} onChange={(e) => setSValue({...svalue, number: e.target.value})}/>
-                            <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
-                                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-                                <Input
-                                    id="standard-adornment-password"
-                                    type={showPassword ? 'text' : 'password'}
-                                    value={svalue.password}
-                                    onChange={(e) => setSValue({...svalue, password: e.target.value})}
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                               
-                                            >
-                                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    }
-                                />
-                            </FormControl>
-                            <Button variant="outlined" className="btn" sx={{color:'#f50000' ,borderColor:'#f50000',margin:'30px'}}>Sign Up</Button>
-                        </div>
-                    </form>
-                </div>
-             
-  )
+      <form id="form2">
+        <h1>Sign Up</h1>
+        <div className="input-container">
+          <TextField
+            sx={{ m: 1, width: "25ch" }}
+            className="input"
+            id="standard-basic"
+            label="Name"
+            variant="standard"
+            value={svalue.name}
+            onChange={(e) => setSValue({ ...svalue, name: e.target.value })}
+          />
+          <TextField
+            sx={{ m: 1, width: "25ch" }}
+            className="input"
+            id="standard-basic"
+            label="Email"
+            variant="standard"
+            value={svalue.email}
+            onChange={(e) => setSValue({ ...svalue, email: e.target.value })}
+          />
+          <TextField
+            sx={{ m: 1, width: "25ch" }}
+            className="input"
+            id="standard-basic"
+            label="Phone No."
+            variant="standard"
+            value={svalue.number}
+            onChange={(e) => setSValue({ ...svalue, number: e.target.value })}
+          />
+          <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
+            <InputLabel htmlFor="standard-adornment-password">
+              Password
+            </InputLabel>
+            <Input
+              id="standard-adornment-password"
+              type={showPassword ? "text" : "password"}
+              value={svalue.password}
+              onChange={(e) =>
+                setSValue({ ...svalue, password: e.target.value })
+              }
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <Button
+            variant="outlined"
+            className="btn"
+            sx={{ color: "#f50000", borderColor: "#f50000", margin: "30px" }}
+          >
+            Sign Up
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- gourmet-app/src/server.js
+++ gourmet-app/src/server.js
@@ -1,14 +1,14 @@
-require('./db/mongoose');
-const express = require('express');
+require("./db/mongoose");
+const express = require("express");
 
-const profileRouter = require("./router/profile")
+const profileRouter = require("./router/profile");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use(profileRouter)
+app.use(express.json());
+app.use(profileRouter);
 
 app.listen(port, () => {
-    console.log("Server is up on port " + port)
-})
+  console.log("Server is up on port " + port);
+});
