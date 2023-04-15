938f62a122bd0643c4aab012b35043dac5f20445
Shubham Mehatre <110455902+shubhamm-26@users.noreply.github.com>
2023-04-15 16:20:44 +0530

    login update

--- gourmet-app/src/App.js
+++ gourmet-app/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import Login from "./Pages/Menu";
+import Login from "./Pages/Login";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Link, Routes,Route } from 'react-router-dom';
 
 const App = () => {
     return (
         <div>
             <Navbar/>
+            <BrowserRouter>
+            <Routes>
+            <Route path="/login" element={<Login />} />
+            </Routes>
+            </BrowserRouter>
         </div>
     );
 };
